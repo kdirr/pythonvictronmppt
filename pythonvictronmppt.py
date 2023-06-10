6be5456a0e0cea1d0bb7513b0d907b9bb177f4ff
Konstantin Dirr <konstantin-dirr@t-online.de>
2023-06-10 17:30:40 +0200

    moved code to pythonvictronmppt.py file

--- pythonvictronmppt.py
+++ pythonvictronmppt.py
@@ -1,3 +1,5 @@
+import serial
+
 
 class pythonvictronmppt:
 
@@ -5,10 +7,47 @@ class pythonvictronmppt:
         self.serial_port = serial_port
         self.serial_speed = speed
     
-    def read(self):
+    
+
+    def get_data(self):
         """
-        Read data from serial console
+        reads data from the serial port the solar charge controller is connected to and filters it after given regex.
+        returns a match object if the regex matched or none if not.
         """
-        
+        with serial.Serial(self.serial_port, self.serial_speed, timeout=None, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE) as s:
+            result = None
+            while not result:
+                result = self.read_serial(s)
+                
+
+    def read_serial(self, conn):
+        byte_sum = 0
+        msg = b''
+        msg_start = False
+        msg_stop = False
+
+        while not msg_stop:
+            # stop reading when reaching checksum field
+            if msg.endswith(b'Checksum\t'):
+                msg_stop = True
+
+            # read single byte and append it to msg
+            byte = conn.read()
+            msg += byte
+
+            # necessary for whatever reason
+            for b in byte:
+                byte_sum += b
+
+            # reset recorded msg when message starts (on first newline)
+            if msg_start == False and msg.endswith(b'\r\n'):
+                msg = b''
+                msg_start = True
+
+        # calculate checksum
+        if (byte_sum % 256) == 0:
+            return msg
+        else:
+            return None
 
 
